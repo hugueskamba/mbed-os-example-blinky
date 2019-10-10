448d70f7f9ea17eb9e5050f5f3f322f336251155
Hugues Kamba <hugues.kamba@arm.com>
2019-10-10 13:43:55 +0100

    Use threads comms to blink and add baremetal version
    The application has been re-written to use inter-thread
    communication in order to blink the LED. It now shows
    how threads can be created, started and how they can
    communicate with each other.
    
    Runtime statistics are made optional via an application
    configuration file. Instructions have been provided to
    use the configuration to enable runtime statistics to
    be printed on the serial output.
    
    Finally, a configuration file has been provided to build
    the application with Mbed OS bare metal profile

--- main.cpp
+++ main.cpp
@@ -1,33 +1,19 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2018 ARM Limited
+ * Copyright (c) 2018-2019 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include "mbed.h"
-#include "ThisThread.h"
-#include "stats_report.h"
+#if !MBED_BARE_METAL
+extern void blinky_rtos();
+#else
+extern void blinky_bare_metal();
+#endif // !MBED_BARE_METAL
 
-DigitalOut led1(LED1);
-
-#define SLEEP_TIME                  500 // (msec)
-#define PRINT_AFTER_N_LOOPS         20
-
-// main() runs in its own thread in the OS
 int main()
 {
-    SystemReport sys_state( SLEEP_TIME * PRINT_AFTER_N_LOOPS /* Loop delay time in ms */);
-
-    int count = 0;
-    while (true) {
-        // Blink LED and wait 0.5 seconds
-        led1 = !led1;
-        ThisThread::sleep_for(SLEEP_TIME);
-
-        if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
-            // Following the main thread wait, report on the current system status
-            sys_state.report_state();
-            count = 0;
-        }
-        ++count;
-    }
+#ifndef MBED_BARE_METAL
+    blinky_rtos();
+#else
+    blinky_bare_metal();
+#endif // !MBED_BARE_METAL
 }
